17057f7ec6a9f3ba8bec0444889fb339c89d65a8
Julian Kern <mail@juliankern.com>
2017-08-10 12:09:16 +0200

    further development

--- client.js
+++ client.js
@@ -1,70 +1,66 @@
-let config;
-let Lcd;
-let lcd;
-let gpio;
-let displayTimeout;
+const utils = global.req('util');
+const gpio = global.req('lib/gpio');
 
-module.exports = (cfg) => {
-    config = cfg;
+module.exports = async (SmartNodeClientPlugin) => {
+    let displayTimeout;
+    let config = SmartNodeClientPlugin.config;
+    let socket = SmartNodeClientPlugin.socket;
+
     console.log('loaded node-home-statusdisplay', config);
-    
-    if ((config.display && config.display.backlight) || config.button) {
-        try {
-            gpio = require('rpi-gpio');
-        } catch(e) {
-            gpio = { 
-                setup: () => {} ,
-                on: () => {},
-                destroy: () => {}
-            }
-        }
-    }
-
-    if (config.display) {
-        Lcd = require('./lib/lcd.js');
-    }
     
     return {
         load,
         unload
     }
-}
-
-function load(socket) {
-    console.log('pluginloaded')
-    socket.emit('pluginloaded');
     
-    if (config.display && config.display.backlight) {
-        gpio.setup(config.display.backlight, gpio.DIR_OUT);
+    async function load() {
+        console.log('pluginloaded')
+        socket.emit('pluginloaded');
+        
+        if (config.display && config.display.backlight) { 
+            gpio.setup(config.display.backlight, gpio.DIR_OUT);
+        }
+        
+        if (config.button) {
+            gpio.on('change', async (channel, value) => {
+                if (channel === config.button && value) {
+                    if (displayTimeout) clearTimeout(displayTimeout);
+                    
+                    await gpio.write(config.display.backlight, true);
+                    
+                    displayTimeout = setTimeout(() => {
+                        gpio.write(config.display.backlight, false);
+                    }, (config.display.timeout || 10) * 1000);
+                }
+            });
+            gpio.setup(config.button, gpio.DIR_IN, gpio.EDGE_BOTH);
+        }
+        
+        if (config.display) {
+            let lcd = new (require('./lib/lcd.js'))(Object.assign({cols: 8, rows: 2}, config.display));
+            
+            socket.on('render', (globals, cb) => {
+                console.log('trying to render display with globals', globals);
+                let lines = config.lines.map((line) => {
+                    return line.replace(/{{([\w\.]+)}}/g, (m, p1) => {
+                        return utils.getValueByPath(globals, p1);
+                    })
+                });
+
+                console.log('render display with lines', lines);
+                // 
+                cb();
+            })
+        }
+
+        return true;
     }
 
-    if (config.display) {
-        lcd = new Lcd(Object.assign({cols: 8, rows: 2}, config.display));
-    }
-    
-    if (config.button) {
-        gpio.on('change', async (channel, value) => {
-            if (channel === config.button && value) {
-                if (displayTimeout) clearTimeout(displayTimeout);
-                
-                await gpio.write(config.display.backlight, true);
-                
-                displayTimeout = setTimeout(() => {
-                    gpio.write(config.display.backlight, false);
-                }, (config.display.timeout || 10) * 1000);
-            }
-        });
-        gpio.setup(config.button, gpio.DIR_IN, gpio.EDGE_BOTH);
-    }
-    
-    socket.on('render', (globals, cb) => {
-        console.log('trying to render display with globals', globals);
-        cb();
-    })
-}
+    function unload() {
+        if (gpio) {
+            gpio.destroy();
+        }
 
-function unload(socket) {
-    if (gpio) {
-        gpio.destroy();
+        return true;
     }
 }
